17496a894fde22f86d8848c00f02c9f17fc32bcb
illy <illy@illyui-MacBookPro.local>
2021-03-20 20:40:20 +0900

    App.js

---
+++ styled-ex/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import TodaysPlan from './TodaysPlan';
+
+class App extends React.Component {
+  render(){
+
+    return(
+      <div>
+        <div><TodaysPlan/></div>
+      </div>
+    );
+  }
+}
+
+
+export default App;

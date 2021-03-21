a63ea11d68ee618bdcb0eb0f39ef21f5b1b064e9
illy <ma_natsu03@naver.com>
2021-03-21 14:55:28 +0900

    new App.js

--- bucket_list/src/App.js
+++ bucket_list/src/App.js
@@ -1,9 +1,13 @@
 import React from "react";
-import logo from "./logo.svg";
-// BucketList 컴포넌트를 import 해옵니다.
+
+import { withRouter } from "react-router";
+import { Route, Switch } from "react-router-dom";
+
 // import [컴포넌트 명] from [컴포넌트가 있는 파일경로];
 import BucketList from "./BucketList";
 import styled from "styled-components";
+import Detail from "./Detail";
+import NotFound from "./NotFound";
 
 // 클래스형 컴포넌트는 이렇게 생겼습니다!
 class App extends React.Component {
@@ -13,56 +17,70 @@ class App extends React.Component {
     this.state = {
       list: ["영화관 가기", "매일 책읽기", "수영 배우기"],
     };
-
+    // ref는 이렇게 선언합니다!
     this.text = React.createRef();
   }
-  handleChange = () =>{
-    let list = this.state.list;
-    const new_item = this.text.current.value;
-    this.setState({list: [...list, new_item]});
-  }
-
 
   componentDidMount() {
     console.log(this.text);
   }
-  
+
+  addBucketList = () => {
+    let list = this.state.list;
+    const new_item = this.text.current.value;
+
+    // 리액트에서는 concat으로 배열항목을 합쳐주는 게 좋아요. 아래처럼요!
+    // list = list.concat(new_item);
+    // this.setState({list: list});
+
+    //  이건 가장 편한 배열 합치기 방법입니다.
+    //  ...은 배열 안에 있는 항목을 전부 꺼내서 늘어놓는다는 뜻입니다. (스프레드 문법이라고 불러요.)
+    this.setState({ list: [...list, new_item] });
+  };
 
   // 랜더 함수 안에 리액트 엘리먼트를 넣어줍니다!
   render() {
-    const list_count = Array.from({length:this.state.list}, (v, i) => i)
-
     return (
       <div className="App">
         <Container>
-          <Title >내 버킷리스트</Title>
-          <Line/>
+          <Title>내 버킷리스트</Title>
+          <Line />
           {/* 컴포넌트를 넣어줍니다. */}
           {/* <컴포넌트 명 [props 명]={넘겨줄 것(리스트, 문자열, 숫자, ...)}/> */}
-          <BucketList list={this.state.list} />
-          <Line />
+          {/* Route 쓰는 법 2가지를 모두 써봅시다! */}
+          <Switch>
+          <Route
+            path="/"
+            exact
+            render={(props) => <BucketList list={this.state.list} history={this.props.history}/>}
+          />
+          <Route path="/detail" component={Detail}/>
+          <Route render={()=>(<NotFound history={this.props.history}/>)} />
+          </Switch>
         </Container>
+        {/* 인풋박스와 추가하기 버튼을 넣어줬어요. */}
         <Input>
-          <input type="text" placeholder="버킷리스트를 작성해주세요" ref={this.text} />
-          <button onClick={this.handleChange}>추가하기</button>
+          <input type="text" ref={this.text} />
+          <button onClick={this.addBucketList}>추가하기</button>
         </Input>
       </div>
     );
   }
 }
+
 const Input = styled.div`
-max-width: 350px;
-min-height: 10vh;
-background-color: #fff;
-padding: 16px;
-margin: 20px auto;
-border-radius: 5px;
-border: 1px solid #ddd;
+  max-width: 350px;
+  min-height: 10vh;
+  background-color: #fff;
+  padding: 16px;
+  margin: 20px auto;
+  border-radius: 5px;
+  border: 1px solid #ddd;
 `;
 
 const Container = styled.div`
   max-width: 350px;
-  min-height: 80vh;
+  min-height: 60vh;
   background-color: #fff;
   padding: 16px;
   margin: 20px auto;
@@ -79,5 +97,5 @@ const Line = styled.hr`
   margin: 16px 0px;
   border: 1px dotted #ddd;
 `;
-
-export default App;
+// withRouter 적용
+export default withRouter(App);
